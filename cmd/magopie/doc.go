92e367333e0ff22b51bba67fb9807bd8cb0dd19e
Jacob Walker <jacobwalker0814@gmail.com>
2016-01-22 22:19:13 -0600

    Start on server

--- cmd/magopie/doc.go
+++ cmd/magopie/doc.go
@@ -1,1 +1,19 @@
+// The magopie server aggregates search results from various torrent sites
+// and downloads specified torrent files.
+//
+// Available routes are:
+//
+// GET    /sites
+// GET    /sites/{id}
+// POST   /sites/{id}/enabled
+// DELETE /sites/{id}/enabled
+
+// GET    /torrents?q=ubuntu
+// GET    /torrents/{id}
+// POST   /torrents/{id}/download
+// DELETE /torrents/{id}/download
+
+// GET    /downloads
+// GET    /downloads/{id}
+// POST   /downloads/{id}
 package main
--- cmd/magopie/main.go
+++ cmd/magopie/main.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	var port string
+	if port = os.Getenv("PORT"); port == "" {
+		port = "8080"
+	}
+
+	addr := fmt.Sprintf("%s:%s", "localhost", port)
+	log.Printf("Listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
+}
