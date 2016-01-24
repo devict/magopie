96c55af0a516b91b6c0a4ee9750fa3dd532e345e
Jacob Walker <jacobwalker0814@gmail.com>
2016-01-24 00:27:35 -0600

    Fix lint errors

--- cmd/magopie/errors.go
+++ cmd/magopie/errors.go
@@ -2,8 +2,16 @@ package main
 
 import "errors"
 
-// ErrFailedRequest is returned when we get a non-200 status from one of our
-// upstream sites.
-var ErrFailedRequest = errors.New("http request had non-200 status")
-var ErrHashlessMagnet = errors.New("could not find hash in magnet URI")
-var ErrCannotFindFileSize = errors.New("could not find tpb filesize in html")
+var (
+	// ErrFailedRequest is returned when we get a non-200 status from one of our
+	// upstream sites.
+	ErrFailedRequest = errors.New("http request had non-200 status")
+
+	// ErrHashlessMagnet is returned when we fail to parse the BTIH hash from a
+	// Magnet URI
+	ErrHashlessMagnet = errors.New("could not find hash in magnet URI")
+
+	// ErrCannotFindFileSize is used when we fail to parse the file size for a
+	// torrent returned from TPB.
+	ErrCannotFindFileSize = errors.New("could not find tpb filesize in html")
+)
